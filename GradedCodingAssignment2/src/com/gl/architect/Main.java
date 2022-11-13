410d4b662d70bedd573bc251ff02827cc2ff88fb
Darshan Sharma <daru8331@gmail.com>
2022-11-13 14:56:21 +0530

    Final commit before submission

---
+++ GradedCodingAssignment2/src/com/gl/architect/Main.java
@@ -0,0 +1,16 @@
+package com.gl.architect;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 Create a Building Architect class
+		 */
+		BuildingArchitect ba = new BuildingArchitect();
+		ba.getInput();
+		ba.performOrderOfConstruction();
+
+	}
+
+}
